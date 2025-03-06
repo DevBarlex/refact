e8b825a94f731db3b813dfa6186e6680c0990308
Guanjino <jjpereiranaranjo@gmail.com>
2025-03-06 17:03:10 +0100

    modulación

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 function Footer() {
   return (
     <footer className="footer">
-      <p>All rights reserved. Cocreta INC ©</p>
-      <p>Address: Simancas (Madrid)</p>
-      <p>Phone: 917335673 - 635444912</p>
+      <p>Todos los derechos reservados.</p>
+      <p>Cocreta INC ©</p>
+      <p>Dirección: Simancas (Madrid)</p>
+      <p>Teléfono: 917335673 - 635444912</p>
       <p>Email: croquetasINC@hotmail.com</p>
     </footer>
   )
